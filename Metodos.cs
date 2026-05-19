cf8986005ff85c7bbb2ddb4fe1c6bcab55bbbc84
rafinha <rafaelamarostegan@gmail.com>
2026-05-19 09:02:09 -0300

    feat: commitando novo exemplo (métodos)

---
+++ Metodos.cs
@@ -0,0 +1,27 @@
+/*
+static void ExibirMenu()
+{
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Mostrar saudação");
+}
+
+static int Somar(int a, int b)
+{
+    return a + b;
+}
+
+static void Saudacao(string nome)
+{
+    Console.WriteLine($"Olá, {nome}");
+}
+
+static void Main()
+{
+    ExibirMenu();
+
+    int resultado = Somar(4, 6);
+    Console.WriteLine($"Resultado da soma: {resultado}");
+
+    Saudacao("Ana");
+}
+*/
